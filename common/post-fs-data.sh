c640569c88190b4ebc8c5480869f1787891353ec
TechnoSparks <shahmi@technosparks.net>
2018-10-05 21:19:37 +0800

    Add -p switch to resetprop, so it should access the persist storage for the props. Effectiveness TBA

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,6 +5,6 @@ MODDIR=${0%/*}
 
 # Enforce crticial build props
 resetprop ro.sys.sdcardfs false
-resetprop persist.sys.sdcardfs force_off
-resetprop persist.esdfs_sdcard false
-resetprop persist.fuse_sdcard true
+resetprop -p persist.sys.sdcardfs force_off
+resetprop -p persist.esdfs_sdcard false
+resetprop -p persist.fuse_sdcard true
