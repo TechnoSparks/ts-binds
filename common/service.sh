cd5bb47d389578e8ed280b40346cd882fb6200aa
TechnoSparks <shahmi@technosparks.net>
2018-10-07 22:40:13 +0800

    Rewrite service.sh for proper logging

--- common/service.sh
+++ common/service.sh
@@ -5,20 +5,18 @@
 MODDIR=${0%/*}
 sdstatus=0
 logfile=/data/media/0/ts-binds.log
-function tslog {
-    echo -e "$(date +'%F %T'):\n  $1\n" >> $logfile
-    echo -e "$1"
-}
+log="tee -a $logfile"
 echo -e "Log initialised at: $(date) \n\n" > $logfile
 
 # Copy over user's bind list ---------------------------
 tsbinds update
 
 # Barrier, do not continue until SD card is mounted ----
+sleep 10
 until [ $sdstatus == "1" ]; do
     if sdname=$(grep -m 1 "/mnt/media_rw/" /proc/mounts | grep -m 1 -Eo "[0-9A-Z]{4}-[0-9A-Z]{4}"); then
         sdstatus=1
-        tslog "sdcard $sdname mounted"
+        echo "sdcard $sdname mounted" | $log
     else
         sleep 1
     fi
@@ -28,9 +26,8 @@ done
 if [ ! -f /data/ts-binds/disable.txt ]; then
     tsbinds bind all
 else
-    tslog "OFF switch file (disable.txt) found. No changes are made."
+    echo "OFF switch file (disable.txt) found. No changes are made." | $log
 fi
 
 # End bind ---------------------------------------------
-tslog "Script execution completed"
-unset -f tslog
+echo "Script execution completed" | $log
