648e9dfa946da55312eb6d4e4b48cd09dfef10f0
TechnoSparks <shahmi@technosparks.net>
2018-10-28 10:18:38 +0800

    Redirect output from stdout to logfile for service.sh

--- common/service.sh
+++ common/service.sh
@@ -7,18 +7,19 @@ datadir=/data/ts-binds
 sdstatus=0
 logfile=$MODDIR/data/ts-binds.log
 logfileuser=/data/media/0/ts-binds.log
-log="tee -a $logfile"
-echo -e "Log initialised at: $(date) \n\n" > $logfile
+exec > $logfile
+exec 2>&1
+echo -e "Log initialised at: $(date) \n\n"
 
 # Copy over user's bind list ---------------------------
-tsbinds update >> $logfile 2>> $logfile
+tsbinds update
 
 # Barrier, do not continue until SD card is mounted ----
 i=0
 until [ $sdstatus == "1" ]; do
     if sdname=$(grep -m 1 "/mnt/media_rw/" /proc/mounts | grep -m 1 -Eo "[0-9A-Z]{4}-[0-9A-Z]{4}"); then
         sdstatus=1
-        echo "sdcard $sdname mounted" | $log
+        echo "sdcard $sdname mounted"
     else
         i=$(expr $i + 1)
         if [ $i -eq 20 ]; then
@@ -32,13 +33,13 @@ done
 
 # Execute ----------------------------------------------
 if [ ! -f $datadir/disable ]; then
-    tsbinds bind all >> $logfile 2>> $logfile
+    tsbinds bind all
 else
-    echo "OFF switch file (disable) found. No changes are made." | $log
+    echo "OFF switch file (disable) found. No changes are made."
 fi
 
 # End bind ---------------------------------------------
-echo "Script execution completed" | $log
+echo "Script execution completed"
 cp -f $logfile $logfileuser
 chown 1023:1023 $logfileuser
 chmod 0664 $logfileuser
